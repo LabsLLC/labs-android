b3ca6c511ce43934a0c790fdcedf455ba1a55cfd
Aura Velarde <aura.velarde@gmail.com>
2017-12-01 00:51:11 -0500

    Added navigation to the email

---
+++ SignUp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {Text} from 'react-native-elements';
+import {View} from 'react-native';
+
+
+export default class SignUp extends Component<{}> {
+
+    render() {
+        return (
+            <View>
+                <Text> For testing </Text>
+            </View>
+        )}
+}
+
+module.exports = SignUp;
