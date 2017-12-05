abf134a77beae0a6bc420328b604b2e98af9e4a8
Aura Velarde <aura.velarde@gmail.com>
2017-12-05 16:41:10 -0500

    Geolocation for both current location and addresses, secret config file added, and not on git

--- app/screens/HomePage.js
+++ app/screens/HomePage.js
@@ -4,7 +4,8 @@ import {ScrollView, Modal, StyleSheet} from 'react-native';
 import {View} from 'react-native';
 import ReactionModal from '../components/ReactionModal/ReactionModal.js'
 import Navbar from '../components/NavBar/Navbar.js'
-
+import Geocoder from 'react-native-geocoding'
+import Secrets from '../config/secrets.js'
 
 export default class HomePage extends Component<{}> {
 
@@ -12,10 +13,37 @@ export default class HomePage extends Component<{}> {
         super(props);
         //function bindings
         this.handleChange = this.handleChange.bind(this);
-
+        Geocoder.setApiKey(Secrets.GoogleApiSecret);
         this.state = {
             searchText:'',
+            latitude: null,
+            longitude: null,
+            error: null,
         };
+    }
+
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    error: null,
+                });
+            },
+            (error) => this.setState({ error: error.message }),
+            { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+        );
+
+        Geocoder.getFromLocation("100 Institute Rd, Worcester, MA").then(
+            json => {
+                var location = json.results[0].geometry.location;
+                console.log(location.lat + ", " + location.lng);
+            },
+            error => {
+                console.log(error);
+            }
+        );
     }
 
     handleChange(event) {
@@ -86,6 +114,12 @@ export default class HomePage extends Component<{}> {
                           titleStyle = {styles.dividerTextStyle}>
 
                     </Card>
+                </View>
+
+                <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>Latitude: {this.state.latitude}</Text>
+                    <Text>Longitude: {this.state.longitude}</Text>
+                    {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
                 </View>
 
             </ScrollView>
