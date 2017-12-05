09e7135b560cc19cacc9834f3dda2fb882c1f99b
Ben Hylak <blhylak@wpi.edu>
2017-12-05 16:19:32 -0500

    Fetching profile picture from Facebook

--- app/screens/UserProfile.js
+++ app/screens/UserProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Button, Text, Tile, SearchBar, List, ListItem} from 'react-native-elements';
-import {Image, ScrollView, Modal, StyleSheet,} from 'react-native';
+import {Image, TouchableOpacity, ScrollView, Modal, StyleSheet, AsyncStorage} from 'react-native';
 import {View} from 'react-native';
 import Navbar from '../components/NavBar/Navbar.js'
 
@@ -14,46 +14,87 @@ export default class UserProfile extends Component<{}> {
 
     constructor(props) {
         super(props);
+
         //function bindings
         this.handleChange = this.handleChange.bind(this);
+        this._responseInfoCallback = this._responseInfoCallback.bind(this);
 
         this.state = {
             searchText: '',
+            profileImage: 'http://placehold.it/120x120',
         };
+    }
+
+    componentDidMount()
+    {
+        AsyncStorage.getItem("profileImage").then((value) => {
+            if(value)
+            {
+                this.setState({"profileImage": value});
+            }
+        }).done();
+
+        //in case the facebook profile picture has been updated, go fetch it
+        this.fetchFbProfilePic()
     }
 
     handleChange(event) {
         this.setState({searchText: event});
     }
 
-    static handleClickExperiment(event) {
-        // console.log("Clicked List item "+event.toString())
-    }
-
     _responseInfoCallback(error: ?Object, result: ?Object) {
         if (error) {
-            alert('Error fetching data: ' + error.toString());
+           // alert('Error fetching data: ' + error.toString());
+            console.error('Error fetching data');
         } else {
-            alert('Success fetching data: ' + result.toString());
+
+            if(this.state.profileImage !== result.data.url)
+            {
+                this.setState({
+                    profileImage: result.data.url
+                });
+
+                AsyncStorage.setItem("profileImage", result.data.url);
+            }
+
+            //alert('Success fetching data: ' + result.toString());
         }
+
+        console.log(result);
     }
 
-    onProfileClick()
+    fetchFbProfilePic()
     {
         const infoRequest = new GraphRequest(
-            '/me',
-            null,
+            "/me/picture",
+            {
+                parameters:
+                    {
+                        type: {
+                            string: 'large'
+                        },
+                        redirect: /* if not specified, an image (non-json) rather than the url is returned */
+                        {
+                            string: 'false'
+                        }
+                    }
+            },
             this._responseInfoCallback,
         );
 
         new GraphRequestManager().addRequest(infoRequest).start();
     }
 
+    onProfileClick()
+    {
+        this.fetchFbProfilePic();
+    }
+
     render() {
         return (
             <View>
-                <TouchableOpacity>
-                    <Image style={styles.image} onClick="onProfileClick" source={{uri: 'http://placehold.it/120x120'}}/>
+                <TouchableOpacity onPress={() => this.onProfileClick()}>
+                    <Image style={styles.image} onClick="onProfileClick" source={{uri: this.state.profileImage}}/>
                 </TouchableOpacity>
             </View>
         )
