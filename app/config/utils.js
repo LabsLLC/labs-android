22522b57feb4ef4724068e2caf6b03ee15a9983d
Andrew Rottier <andrewrottier95@gmail.com>
2017-12-13 14:39:22 -0500

    Added Journal Photo and path

--- app/config/utils.js
+++ app/config/utils.js
@@ -1,18 +1,24 @@
 import WakeUpImagePath from '../images/experiments/wakeupearly.png';
 import ExerciseImagePath from '../images/experiments/exercise.png';
 import MeditationImagePath from '../images/experiments/meditation.png';
+import JournalImagePath from '../images/experiments/journal.png';
+
 
 
 export default class ExperimentImages {
     static WakeUpImage = WakeUpImagePath;
     static ExerciseImage = ExerciseImagePath;
     static MeditationImage = MeditationImagePath;
+    static JournalImage = JournalImagePath;
 
     static getImage(name){
 
         let image = null;
 
         switch(name) {
+            case "Journaling":
+                image = this.JournalImage;
+                break;
             case "Wake up early":
                 image = this.WakeUpImage;
                 break;
